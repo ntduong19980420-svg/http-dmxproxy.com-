2aa97f0413c50d66344d47f4d2598a4463c214d5
ntduong19980420-svg <ntduong19980420@gmail.com>
2026-08-25 20:08:04 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -7,43 +7,52 @@ PROXYRACK_HOST = "premium.residential.proxyrack.net:10000"
 PROXYRACK_USER = "gojyxogosutase"
 PROXYRACK_PASS = "WPYV6U0-PXC4X1B-5KJAKQA-IAHTEI6-FPBHKYI-BI7X1IT-LRYHKBZ"
 
-ALL_COUNTRIES = ['US', 'VN', 'FR', 'GB', 'DE', 'JP', 'KR', 'CA', 'AU', 'SG', 'IN', 'TH', 'AR', 'BR', 'ES', 'IT']
-
+ALL_COUNTRIES = [
+    'AF', 'AL', 'DZ', 'AS', 'AD', 'AO', 'AI', 'AQ', 'AG', 'AR', 'AM', 'AW', 'AU', 'AT', 'AZ',
+    'BS', 'BH', 'BD', 'BB', 'BY', 'BE', 'BZ', 'BJ', 'BM', 'BT', 'BO', 'BA', 'BW', 'BR', 'BN',
+    'BG', 'BF', 'BI', 'KH', 'CM', 'CA', 'CV', 'KY', 'CF', 'TD', 'CL', 'CN', 'CO', 'KM', 'CG',
+    'CD', 'CK', 'CR', 'CI', 'HR', 'CU', 'CY', 'CZ', 'DK', 'DJ', 'DM', 'DO', 'EC', 'EG', 'SV',
+    'GQ', 'ER', 'EE', 'ET', 'FJ', 'FI', 'FR', 'GA', 'GM', 'GE', 'DE', 'GH', 'GI', 'GR', 'GL',
+    'GD', 'GU', 'GT', 'GN', 'GW', 'GY', 'HT', 'HN', 'HK', 'HU', 'IS', 'IN', 'ID', 'IR', 'IQ',
+    'IE', 'IL', 'IT', 'JM', 'JP', 'JO', 'KZ', 'KE', 'KI', 'KP', 'KR', 'KW', 'KG', 'LA', 'LV',
+    'LB', 'LS', 'LR', 'LY', 'LI', 'LT', 'LU', 'MO', 'MK', 'MG', 'MW', 'MY', 'MV', 'ML', 'MT',
+    'MH', 'MR', 'MU', 'MX', 'FM', 'MD', 'MC', 'MN', 'ME', 'MA', 'MZ', 'MM', 'NA', 'NR', 'NP',
+    'NL', 'NZ', 'NI', 'NE', 'NG', 'NO', 'OM', 'PK', 'PW', 'PA', 'PG', 'PY', 'PE', 'PH', 'PL',
+    'PT', 'PR', 'QA', 'RO', 'RU', 'RW', 'KN', 'LC', 'VC', 'WS', 'SM', 'ST', 'SA', 'SN', 'RS',
+    'SC', 'SL', 'SG', 'SK', 'SI', 'SB', 'SO', 'ZA', 'ES', 'LK', 'SD', 'SR', 'SZ', 'SE', 'CH',
+    'SY', 'TW', 'TJ', 'TZ', 'TH', 'TL', 'TG', 'TO', 'TT', 'TN', 'TR', 'TM', 'TV', 'UG', 'UA',
+    'AE', 'GB', 'US', 'UY', 'UZ', 'VU', 'VE', 'VN', 'YE', 'ZM', 'ZW'
+]
 
 @app.route('/')
 def home():
-  return render_template('index.html')
-
+    return render_template('index.html')
 
 @app.route('/generate', methods=['POST'])
 def generate():
-  data = request.get_json() or {}
-  selected_country = data.get('country', 'ALL').upper()
-  os_name = data.get('os', 'Windows')
+    data = request.get_json() or {}
+    selected_country = data.get('country', 'ALL').upper()
+    os_name = data.get('os', 'Windows')
 
-  try:
-    qty = int(data.get('qty', 10))
-  except (ValueError, TypeError):
-    qty = 10
+    try:
+        qty = int(data.get('qty', 10))
+    except (ValueError, TypeError):
+        qty = 10
 
-  qty = max(1, min(qty, 100))
+    qty = max(1, min(qty, 100))
 
-  proxies = []
-  for _ in range(qty):
-    if selected_country == 'ALL' or not selected_country:
-      country = random.choice(ALL_COUNTRIES)
-    else:
-      country = selected_country
+    proxies = []
+    for _ in range(qty):
+        if selected_country == 'ALL' or not selected_country:
+            country = random.choice(ALL_COUNTRIES)
+        else:
+            country = selected_country
 
-    sess = ''.join(
-        random.choices('abcdefghijklmnopqrstuvwxyz0123456789', k=12)
-    )
+        sess = ''.join(random.choices('abcdefghijklmnopqrstuvwxyz0123456789', k=12))
+        proxy_str = f'{PROXYRACK_HOST}:{PROXYRACK_USER}-country-{country}-session-{sess}-osName-{os_name}:{PROXYRACK_PASS}'
+        proxies.append(proxy_str)
 
-    proxy_str = f'{PROXYRACK_HOST}:{PROXYRACK_USER}-country-{country}-session-{sess}-osName-{os_name}:{PROXYRACK_PASS}'
-    proxies.append(proxy_str)
-
-  return jsonify({'proxies': proxies})
-
+    return jsonify({'proxies': proxies})
 
 if __name__ == '__main__':
-  app.run()
+    app.run()
